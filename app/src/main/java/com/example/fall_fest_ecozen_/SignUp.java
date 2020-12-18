e98575611f301c89985228a31de17886b1e5c8fa
eeshaydv <eeshaydv@gmail.com>
2020-12-18 17:31:32 +0530

    signup with number implemented

--- app/src/main/java/com/example/fall_fest_ecozen_/SignUp.java
+++ app/src/main/java/com/example/fall_fest_ecozen_/SignUp.java
@@ -1,6 +1,37 @@
 package com.example.fall_fest_ecozen_;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUp extends Activity {
+    private EditText editTextMobile;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        editTextMobile = findViewById(R.id.editTextMobile);
+        findViewById(R.id.buttonContinue).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String mobile = editTextMobile.getText().toString().trim();
+
+                if(mobile.isEmpty() || mobile.length() < 10){
+                    editTextMobile.setError("Enter a valid mobile");
+                    editTextMobile.requestFocus();
+                    return;
+                }
+
+                Intent intent = new Intent(SignUp.this, otp_screen.class);
+                intent.putExtra("mobile", mobile);
+                startActivity(intent);
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/fall_fest_ecozen_/Splash.java
+++ app/src/main/java/com/example/fall_fest_ecozen_/Splash.java
@@ -15,7 +15,7 @@ public class Splash extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Splash.this, MainActivity.class);
+                Intent intent = new Intent(Splash.this, SignUp.class);
                 startActivity(intent);
                 finish();
             }
