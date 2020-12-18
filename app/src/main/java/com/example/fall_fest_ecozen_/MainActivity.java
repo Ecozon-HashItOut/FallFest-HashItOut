088833be8e6c3407e9d9ca2538f432a9ead109b4
Pranitha <pranithas2001@gmail.com>
2020-12-18 09:33:02 +0530

    Initial-2

--- app/src/main/java/com/example/fall_fest_ecozen_/MainActivity.java
+++ app/src/main/java/com/example/fall_fest_ecozen_/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.fall_fest_ecozen_;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button signin,google;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signin=findViewById(R.id.letsgetst);
+        google=findViewById(R.id.conwithg);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,SignIn.class);
+                startActivity(intent);
+            }
+        });
     }
 }
